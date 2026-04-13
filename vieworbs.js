4c9fc74103ed9feb7b380b10e1971939f0a80260
= <dolin.37@osu.edu>
2026-04-13 15:44:42 -0400

    Updated to include the CP- molecule for Homework 10. Also made somewhat dynamic dropdown menus.

--- vieworbs.js
+++ vieworbs.js
@@ -1,6 +1,6 @@
 function jmolReady(applet) {
-    // Jmol.script(myJmol, 'set echo middle center; echo "Drag and drop a .molden file here to visualize.";' );
-    Jmol.script(myJmol, "load files mol/n2_321g.molden; rotate best; mo titleformat 'MO %I/%N | Energy = %E %U | Occupancy=%O'")
+    Jmol.script(myJmol, 'set echo middle center; echo "Select a molecule to visualize.";' );
+    // Jmol.script(myJmol, "load files mol/n2_321g.molden; rotate best; mo titleformat 'MO %I/%N | Energy = %E %U | Occupancy=%O'")
 }
 
 var myJmol = "myJmol";
@@ -14,13 +14,31 @@ var Info = {
     readyFunction: jmolReady,
 }
 
-function LoadMOs(mo) {
-    Jmol.script(myJmol, "load files " + mo)
+let numOrbs;
+
+function loadMOs(moldenfile) {
+    Jmol.script(myJmol, "load files " + moldenfile + "; rotate best; mo titleformat 'MO %I/%N | Energy = %E %U | Occupancy=%O'")
 }
 
-function selectMO(num) {
-    // let e = $("#num").val();
+function selectMO_updatenum(num) {
     Jmol.script(myJmol, 'mo ' + num)
+    document.querySelector("morb").value = num
+}
+function selectMO(num) {
+    Jmol.script(myJmol, 'mo ' + num)
+}
+
+function createDropdown(dropdown_name, num_options) {
+    let newOption, i;
+    const select = document.getElementById(dropdown_name);
+    while (select.lastElementChild.id !== 'default') {  // remove all existing elements except for default message. Adapted from https://stackoverflow.com/a/19885871
+        select.removeChild(select.lastChild);
+    }
+    for (i = 1; i <= num_options; i++) {
+        newOption = document.createElement('option');
+        newOption.textContent = String(i);
+        select.appendChild(newOption);
+    }
 }
 
 $(document).ready(function() {
